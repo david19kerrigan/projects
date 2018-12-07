5d19503dd519b874804875366f71b103ad822b1d
unknown <david19kerrigan@gmail.com>
2018-12-07 14:25:15 -0500

    a

---
+++ hde/one.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func getTestCase(i int) {
+	if i > 0 {
+		fmt.Print("first line(X): ")
+		var text string
+		fmt.Scanln(&text)
+		n, _ := strconv.Atoi(text)
+
+		fmt.Print("test cases(Yn): ")
+		allNums := make([]int, n)
+		readNumbers(allNums, 0, n)
+
+		fmt.Println("total:", sumNums(allNums, 0, n, 0))
+
+		fmt.Scan(&allNums)
+		fmt.Scanln(&text)
+		getTestCase(i - 1)
+	}
+}
+
+func sumNums(nums []int, pos int, length int, sum int) int {
+	if pos < length {
+		if nums[pos] > 0 {
+
+			return sumNums(nums, pos+1, length, sum+nums[pos]*nums[pos])
+
+		}
+		if nums[pos] <= 0 {
+			return sumNums(nums, pos+1, length, sum)
+		}
+
+	}
+
+	return sum
+}
+
+func readNumbers(allNums []int, i, j int) {
+	if j == 0 {
+		return
+	}
+	fmt.Scan(&allNums[i])
+	readNumbers(allNums, i+1, j-1)
+}
+
+func main() {
+
+	fmt.Print("first line(N): ")
+	var text string
+	fmt.Scanln(&text)
+	j, _ := strconv.Atoi(text)
+
+	getTestCase(j)
+}
